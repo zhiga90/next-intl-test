2482ff5ab1f1158fa91bd703e5bd0b26043e2935
Zhalgas Azhibayev <zhiga_@mail.ru>
2024-06-12 09:04:03 +0500

    next-intl

---
+++ src/app/en/layout.tsx
@@ -0,0 +1,32 @@
+import { NextIntlClientProvider } from "next-intl";
+import { unstable_setRequestLocale, getMessages } from "next-intl/server";
+
+// Can be imported from a shared config
+const locales = ["en", "ru"];
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/ru/page.tsx
@@ -0,0 +1,24 @@
+import { useTranslations } from "next-intl";
+
+import { unstable_setRequestLocale, getTranslations } from "next-intl/server";
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    title: t("title"),
+  };
+}
+
+export default function Index({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("Index");
+  return <h1>{t("title")}</h1>;
+}
